4fb7b78841947f3d528cf14f22a6733e7634f4da
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-12 02:24:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+var el = document.querySelector('main#main');
+el.remove();
+
+let element = document.createElement('h1');
+element.setAttribute("id", "victory");
+document.body.appendChild(element); 
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = 
+
